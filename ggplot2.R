7c142f92da905dc675d56e716e73822b5ef7c02a
T.zhong16 <t.zhong16@gmail.com>
2016-12-02 22:20:50 +1300

    updated ggplot2

--- ggplot2.R
+++ ggplot2.R
@@ -10,3 +10,41 @@ ggplot(mtcars, aes(x = factor(cyl), y = mpg,)) +
   
 ggplot(mtcars, aes(x = wt, y = mpg)) +
   geom_point()
+  
+ggplot(mtcars, aes(x = wt, y = mpg, col = disp)) +
+  geom_point()
+  
+ggplot(mtcars, aes(x = wt, y = mpg, size = disp)) +
+  geom_point()
+  
+str(diamonds)
+
+ggplot(diamonds, aes(x = carat, y = price)) +
+   geom_point()
+   
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_point() +
+  geom_smooth()
+  
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_point() +
+  geom_smooth()
+  
+ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
+  geom_smooth()
+  
+ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
+  geom_smooth()
+
+ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
+  geom_point(alpha = 0.4) 
+  
+ dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
+ 
+ dia_plot <- ggplot(diamonds, aes(x = carat, y = price))+
+               geom_point()
+               
+dia_plot <- ggplot(diamonds, aes(x = carat, y = price))+
+               geom_point(aes(col = clarity))
+               
+                            
